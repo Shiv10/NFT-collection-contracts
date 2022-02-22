6e6cf29bf10b4228002ecba5e55180e4083f2f7e
Shiv10 <shivanshsharma2012@gmail.com>
2022-02-22 16:35:08 +0530

    feat: add customize feature to mint nft

--- scripts/run.js
+++ scripts/run.js
@@ -1,7 +1,11 @@
 const main = async () => {
+
     const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
+
+    let txn = await nftContract.mintNFT("#000", "#fff", {value: hre.ethers.utils.parseEther('100')});
+    await txn.wait();
     
     console.log("Contract deployed to address: ", nftContract.address);
 }
